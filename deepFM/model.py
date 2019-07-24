e256e52444f8a43b1da9732f2be0cac72d3f5482
SnowIsWhite <jibae617@gmail.com>
2019-07-24 14:53:23 +0900

    added lambda layer

--- deepFM/model.py
+++ deepFM/model.py
@@ -19,7 +19,7 @@ def model(embedding_size, field_vocab_size=[], hidden_units=[4,4,4], dropout=0.5
     embed_list = layers.concatenate(embed_list, axis=1) # none, F, K
 
     fm_first_in = layers.concatenate(embed_one_list, axis=1) # None, F, 1
-    fm_first_in = backend.squeeze(fm_first_in, axis=2) # none, F
+    fm_first_in = layers.Lambda(lambda x: backend.squeeze(x, axis=2))(fm_first_in)
 
     # dense layer
     dropouts = [dropout] * len(hidden_units)
@@ -29,18 +29,18 @@ def model(embedding_size, field_vocab_size=[], hidden_units=[4,4,4], dropout=0.5
     for i, (h, d) in enumerate(zip(hidden_units, dropouts)):
         z = layers.Dense(units=h, kernel_initializer=weight_init)(deep_in)
         z = layers.BatchNormalization(axis=-1)(z)
-        z = keras.activations.relu(z)
+        z = layers.Activation("relu")(z)
         z = layers.Dropout(d,seed=d * i)(z) if d > 0 else z
     deep_out = layers.Dense(units=1, activation=tf.nn.softmax, kernel_initializer=weight_init)(z)
     # deep_out: None, 1
 
     # fm layer
-    fm_first_order = backend.sum(fm_first_in, axis=1) #None, 1
+    fm_first_order = layers.Lambda(lambda x: backend.sum(x, axis=1))(fm_first_in) #None, 1
 
-    emb_sum_squared = backend.square(backend.sum(embed_list, axis=1)) #none, K
-    emb_squared_sum = backend.sum(backend.square(embed_list), axis=1) #none, K
+    emb_sum_squared = layers.Lambda(lambda x: backend.square(backend.sum(x, axis=1)))(embed_list) #none, K
+    emb_squared_sum = layers.Lambda(lambda x: backend.sum(backend.square(x), axis=1))(embed_list) #none, K
     fm_second_order = layers.Subtract()([emb_sum_squared, emb_squared_sum])
-    fm_second_order = backend.sum(fm_second_order, axis=1) #none, 1
+    fm_second_order = layers.Lambda(lambda x: backend.sum(x, axis=1))(fm_second_order) #none, 1
     fm_out = layers.Add()([fm_first_order, fm_second_order])
 
     out = layers.Add()([deep_out, fm_out])
