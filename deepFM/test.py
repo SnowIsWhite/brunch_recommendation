f3a8b7af52908fe0364fad976f44d51e5c9ff08d
SnowIsWhite <jibae617@gmail.com>
2019-07-24 18:51:28 +0900

    test

--- deepFM/test.py
+++ deepFM/test.py
@@ -5,6 +5,7 @@ import os
 from sklearn.model_selection import train_test_split
 from data_generation import load_data, get_field_vocab_size
 from model import model
+from recommendation import final_recommendation, write_recommendation_file
 
 target_df = './dataframe/test_df_2.csv'
 model_name = './trained_model1.h5'
@@ -21,3 +22,8 @@ Y=df.iloc[:,-1]
 m = tf.keras.models.load_model(model_name)
 
 result = m.evaluate(X,Y)
+# change to final dic, user_order list
+user_order = []
+result_dic = {}
+final = final_recommendation(result_dic)
+write_recommendation_file(user_order, final)
